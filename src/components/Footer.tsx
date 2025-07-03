76ee73139dce2f54c243fb13cbcbfe2fa75eacce
Ondrej Babinský <babinskyondrej@gmail.com>
2025-07-03 15:27:03 +0200

    setup base file structure add nav add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="fixed bottom-0 w-full text-center text-sm text-muted-foreground py-2 bg-background border-t">
+            © 2025 Ondrej Babinský
+        </footer>
+    )
+}
